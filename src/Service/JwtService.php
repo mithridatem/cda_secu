9d70045b7164dd7fd8943b3fd91e2b1e5ff23a71
mithridatem <mathieumithridate@adrar-formation.com>
2023-11-02 16:13:49 +0100

    Méthode pour générer le token

--- src/Service/JwtService.php
+++ src/Service/JwtService.php
@@ -31,4 +31,32 @@ class JwtService{
             return false;
         }
     }
+    public function genToken($email){
+        //construction du JWT
+        require_once('../vendor/autoload.php');
+        //Variables pour le token
+        $issuedAt   = new \DateTimeImmutable();
+        $expire     = $issuedAt->modify('+60 minutes')->getTimestamp();
+        $serverName = "your.domain.name";
+        $userName   = $this->userRepository->findOneBy(['email'=>$email])->getName();
+        $userFirstname   = $this->userRepository->findOneBy(['email'=>$email])->getFirstname();
+        $id   = $this->userRepository->findOneBy(['email'=>$email])->getId();
+        //Contenu du token
+        $data = [
+            'iat'  => $issuedAt->getTimestamp(),         // Timestamp génération du token
+            'iss'  => $serverName,                       // Serveur
+            'nbf'  => $issuedAt->getTimestamp(),         // Timestamp empécher date antérieure
+            'exp'  => $expire,                           // Timestamp expiration du token
+            'userName' => $userName,
+            'userFirstName' => $userFirstname,
+            'userId' => $id,                    
+        ];
+        //retourne le JWT token encode
+        $token = JWT::encode(
+            $data,
+            $this->token,
+            'HS512'
+        );
+        return $token;
+    }
 }
